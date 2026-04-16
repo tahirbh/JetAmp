aecbf056fd8b6290a79e100b9b72424ce5654c2e
tahirbh <tahirfarooq2@gmail.com>
2026-04-16 22:54:47 +0300

    build

---
+++ app/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.tahirbh.jetamp',
+  appName: 'JetAmp',
+  webDir: 'dist'
+};
+
+export default config;
